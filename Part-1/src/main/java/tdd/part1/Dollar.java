13336801727a601807b12b38f39cfa656a613763
Marc Grimpo <mgrimpo.dev@gmail.com>
2020-07-02 10:29:26 +0200

    Add TODO comments for remaining tasks

--- Part-1/src/main/java/tdd/part1/Dollar.java
+++ Part-1/src/main/java/tdd/part1/Dollar.java
@@ -1,7 +1,10 @@
 package tdd.part1;
 
+// TODO: $5 + 10 CHF = $10 if rate is 2:1
+// TODO: Money rounding?
 public class Dollar {
 
+  // TODO: make "amount" private
   public int amount;
 
   public Dollar(int amount) {
